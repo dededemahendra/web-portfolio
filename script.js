b108b2f7e0280c87976b4653f5e8945622a36cf3
dededemahendra <dededemahendra@gmail.com>
2022-07-23 15:42:59 +0800

    Navbar

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".nav-list");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("active");
+};
+
+ScrollReveal().reveal(".home-text .about-text .about-img .portfolio-text .portfolio-img .service-text .service-img .contact", { delay: 350, origin: "bottom" });
